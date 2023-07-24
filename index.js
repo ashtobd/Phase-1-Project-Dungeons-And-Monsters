60f8e5266d6c1367a69a52d47eaf46730dca5b58
Brian Ashton <ashtobd@gmail.com>
2023-07-24 10:40:30 -0500

    change html

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 //API https://api.open5e.com/v1/monsters
-fetch("https://api.open5e.com/v1/monsters").then(res => res.json()).then(data => {})
+
+const monsterDataTemplate = doucument.querySelector("[monster-data-template")
+const monsterContainer = document.querySelector("[monster-container]")
+
+fetch("https://api.open5e.com/v1/monsters")
+.then(res => res.json())
+.then(data => {
+    console.log(data)
+})
